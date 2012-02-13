6c69d9d9ab1c79b6f8e8867a3ec76edf53c3b84f
Cedric Chabanois <cchabanois@gmail.com>
2012-02-13 07:51:51 +0100

    better tests

--- src/test/java/jenkins/plugins/ivyreport/IvyReportGeneratorTest.java
+++ src/test/java/jenkins/plugins/ivyreport/IvyReportGeneratorTest.java
@@ -8,21 +8,41 @@ import jenkins.plugins.ivyreport.utils.TestAreaUtils;
 
 import org.jvnet.hudson.test.HudsonTestCase;
 
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
+
 public class IvyReportGeneratorTest extends HudsonTestCase {
 	private static final File RESOLUTION_CACHE_ROOT = new File(
 			"resources/testResolutionCache");
 	private static final String RESOLVE_ID = "entropysoft-test";
 	private IvyReportGenerator generator;
+	private File targetDir;
 
 	public void setUp() throws Exception {
 		super.setUp();
+		targetDir = TestAreaUtils.getNonExistingFileInTestArea("target");
 		generator = new IvyReportGenerator(hudson, RESOLVE_ID, new String[] {
-				"compile", "default" }, RESOLUTION_CACHE_ROOT,
-				TestAreaUtils.getNonExistingFileInTestArea("target"));
+				"compile", "default" }, RESOLUTION_CACHE_ROOT, targetDir);
 	}
 
 	public void testReportGenerator() throws IOException, InterruptedException {
-		File file = generator.generateReports();
-		assertTrue(file.exists());
+
+		// When
+		generator.generateReports();
+
+		// Then
+		File compileHtmlFile = new File(targetDir,
+				"entropysoft-test-compile.html");
+		assertTrue(compileHtmlFile.exists());
+		assertTrue(Files.toString(compileHtmlFile, Charsets.UTF_8).contains(
+				"<img src=\"entropysoft-test-compile.svg\">"));
+		assertTrue(new File(targetDir, "entropysoft-test-compile.svg").exists());
+		File defaultHtmlFile = new File(targetDir,
+				"entropysoft-test-default.html");
+		assertTrue(defaultHtmlFile.exists());
+		assertTrue(Files.toString(defaultHtmlFile, Charsets.UTF_8).contains(
+				"<img src=\"entropysoft-test-default.svg\">"));
+		assertTrue(new File(targetDir, "entropysoft-test-default.svg").exists());
 	}
+
 }
